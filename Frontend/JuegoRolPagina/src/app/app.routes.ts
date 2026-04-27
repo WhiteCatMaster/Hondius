5e0182d16d5de462fe2a327d42203bf02c66c440
Aitor Sagarduy <aitor.sagarduy@opendeusto.es>
2026-04-28 01:45:37 +0200

    Arreglos menores en creación de partida y en las rutas

--- Frontend/JuegoRolPagina/src/app/app.routes.ts
+++ Frontend/JuegoRolPagina/src/app/app.routes.ts
@@ -8,16 +8,17 @@ import { UsuarioWebComponent } from './usuario/usuarioWeb';
 import { SelectorMasterComponent} from './selector-master/selector-master.component'
 import { EditarPersonaje } from './editar-personaje/editar-personaje';
 import { LoginComponent } from './login/login.component';
-import { LanzadorDadosComponent } from './lanzador-dados/lanzador-dados.component';
+//import { LanzadorDadosComponent } from './lanzador-dados/lanzador-dados.component';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'login', component: LoginComponent },
   { path: 'crear-partida', component: OpcionesComponent },
   { path: 'selector/:id', component: SelectorComponent },
+  { path: 'selector-personaje/:id', component: SelectorPersonajeComponent },
   { path: 'jugar-combate/:id', component: CombateComponent },
   { path: 'perfil', component: UsuarioWebComponent },
   { path: 'selector-master', component: SelectorMasterComponent },
   { path: 'editar-personaje/:nombre', component: EditarPersonaje },
-  { path: 'dado', component: LanzadorDadosComponent },
+  //{ path: 'dado', component: LanzadorDadosComponent },
 ];
