a619d93125903c2fbb7003084b11868ef02967aa
Eder Mamani <eder.mamani@opendeusto.es>
2026-04-27 19:18:57 +0200

    Pantalla de login creada

--- Frontend/JuegoRolPagina/src/app/app.routes.ts
+++ Frontend/JuegoRolPagina/src/app/app.routes.ts
@@ -7,9 +7,11 @@ import { SelectorPersonajeComponent } from './selectorPersonaje/selector.compone
 import { UsuarioWebComponent } from './usuario/usuarioWeb';
 import { SelectorMasterComponent} from './selector-master/selector-master.component'
 import { EditarPersonaje } from './editar-personaje/editar-personaje';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: Landing },
+  { path: 'login', component: LoginComponent},
   { path: 'crear-partida', component: OpcionesComponent },
   { path: 'selector/:id', component: SelectorComponent },
   { path: 'jugar-combate/:id', component: CombateComponent },
--- Frontend/JuegoRolPagina/src/app/login/login.component.ts
+++ Frontend/JuegoRolPagina/src/app/login/login.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
 import { FirebaseAuthService } from '../../auth/firebase-auth.service';
-
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
-  template: `<button (click)="login()">Login with Google</button>`,
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private firebaseAuth: FirebaseAuthService) {}
+  constructor(private firebaseAuth: FirebaseAuthService,
+    private router: Router
+  ) {}
 
   async login() {
-    const idToken = await this.firebaseAuth.signInWithGoogle();
-    console.log('Firebase ID token:', idToken);
-    // Next step: send token to backend
+   try {
+      const idToken = await this.firebaseAuth.signInWithGoogle();
+      console.log('Firebase ID token:', idToken);
+      
+      this.router.navigate(['/']); 
+      
+    } catch (error) {
+      console.error('El login fue cancelado o hubo un error:', error);
+      alert('Debes identificarte con Google para continuar la aventura.');
+    }
   }
 }
